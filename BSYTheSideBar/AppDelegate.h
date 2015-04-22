0c6e06b16176c26a11b1dbf9db6d6caae61caa63
baishiyun <baishiyun@163.com>
2015-04-22 08:59:49 +0800

    Initial Commit

---
+++ BSYTheSideBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSYTheSideBar
+//
+//  Created by mac on 15/4/22.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSYTheSideBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSYTheSideBar
+//
+//  Created by mac on 15/4/22.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
